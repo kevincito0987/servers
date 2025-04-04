49f7e2e5945f9f1fb6d34592527270b914e606bf
kevincito0987 <kevinangaritacaamano@gmail.com>
2025-04-04 16:02:10 -0500

    perf: :zap: Listo el segundo  ejercicio de los gets con filtrado de correo electronico por medio de los dominios.

--- js/apis.js
+++ js/apis.js
@@ -25,4 +25,21 @@ const getPostsByRange = async (range) => {
   console.log("Este es el resultado de todos los datos: ", result);
   return result;
 };
-getPostsByRange(Number(prompt("Introduce el rango de posts")));
+// getPostsByRange(Number(prompt("Introduce el rango de posts")));
+
+//Crear una funcion que me filtre todos los correos electrónicos segun la terminacion de cada correo por ejemplo .com o .net, esa terminacion se evaluara desde que el usuario introduzca el nombre de dominio y solo se mostrara la lista de correos que terminan con esa terminacion.
+
+const getEmailsByDomain = async (domain) => {
+  const url = `https://jsonplaceholder.typicode.com/comments`;
+  const config = {
+    method: "GET",
+  };
+  const response = await fetch(url, config);
+  const result = await response.json();
+  console.log("Este es el resultado de todos los datos: ", result);
+
+  const getEmailsByDomain = result.filter((email) => email.email.endsWith(domain));
+  console.log("Los correos de la api que terminan con: ", domain, " son: ", getEmailsByDomain);
+};
+
+getEmailsByDomain(prompt("Introduce el dominio"));
